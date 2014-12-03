1f1d897561876ec427441dfbd487bc480bd18dd5
Kim <Kimmazur@users.noreply.github.com>
2014-12-03 18:59:10 +0200

    Update main.c
    ReBuild

--- Time/main.c
+++ Time/main.c
@@ -3,15 +3,13 @@
 
 int main()
 {
-    int h, m, s;/**< 'h' is hours, 'm' is minutes, 's' is seconds */
+    int h, m, s, h1, m1, s1, h2, m2, s2;/**< 'h' is hours, 'm' is minutes, 's' is seconds */
     printf("Type 1st time\n");
     scanf("%d %d %d",&h,&m,&s);
-    printf("%0.2d:%0.2d:%0.2d\n",h,m,s);
     printf("Type 2nd time:\n");
-    scanf("%d %d %d",&h,&m,&s);
-    printf("%0.2d:%0.2d:%0.2d\n",h,m,s);
+    scanf("%d %d %d",&h1,&m1,&s1);
     printf("Type 3rd time:\n");
-    scanf("%d %d %d",&h,&m,&s);
-    printf("%0.2d:%0.2d:%0.2d\n",h,m,s);
+    scanf("%d %d %d",&h2,&m2,&s2);
+    printf("%0.2d:%0.2d:%0.2d\n%0.2d:%0.2d:%0.2d\n%0.2d:%0.2d:%0.2d\n",h,m,s,h1,m1,s1,h2,m2,s2);
     return 0;
 }
