81404da9db8512e39240615c249510f092b98b5a
Kim <Kimmazur@users.noreply.github.com>
2014-12-13 14:47:40 +0300

    Update main.c
    In Visual Studio... shit, anyway(

--- Evklid/main.c
+++ Evklid/main.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
-    unsigned int a, b, r=a/b, i;
-    printf("Type 1st number:\n",a);
-    scanf("%d",&a);
-    printf("Type 2nd number:\n",b);
-    scanf("%d",&b);
-    do
-    {
-        if(a>0&&b>=0)
-        {
-           a/b=r;// Error in here, WTF it want of me?
-           a=b;
-           b=r;
-           ++i
+	int a, b, r;
+	printf("Type 'a':\n");
+	scanf("%d", &a);
+	printf("Type 'b':\n");
+	scanf("%d", &b);
+	while ((a != 0) && (b != 0))
+	{
+		if (a > b)
+		{
+			a=a%b;
+			r = a;
+		}
+		else
+		{
+			b=b%a;
+			r = b;
+		}
+	}
 
-        }
-        else
-        {
-            break;
-        }
-    }
-    while(r!=0);
+	printf("GCD of 'a' and 'b' is %d", r);
+	scanf("%d", &a);
+	getc(stdin);
 
-    return 0;
+	return 0;
 }
