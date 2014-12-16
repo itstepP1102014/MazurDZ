669c0ecdfa98d5f9c02774036cc30404d1d48f8b
Kimmazur <holygun187@gmail.com>
2014-12-16 22:39:15 +0200

    HW#5

--- HW#5/Simple numbers/main.c
+++ HW#5/Simple numbers/main.c
@@ -4,21 +4,24 @@
 int main()
 {
     int n;
-    printf("Type 'n' to check it. It should be less or equal '2':\n");
+    printf("Type 'n' to check it. It should be more or equal '2':\n");
     scanf("%d", &n);
-    if(n<2)
+    do
     {
-        printf("It should be LESS or EQUAL '2'");
+        printf("It should be MORE or EQUAL '2'");
     }
-    else
+    while(n<2);
+    int i=1, d=0; // d - is denominator//
+    for(int i, d; d<=2, i<=n, ++i )
     {
-        while((n=(n%n)=1) && (n=(n/1)=n))
+        if(n%i==0)
         {
+            ++d;
             printf("It's simple number\n");
         }
         else
         {
-            printf("It's not a simple number");
+            printf("It's not a simple number\n");
         }
     }
     return 0;
