c0827b4e45c95cc2286fe149a71dda990469a33f
Kimmazur <holygun187@gmail.com>
2015-01-12 18:26:34 +0200

    HW#6

--- HW#6/Quest/main.c
+++ HW#6/Quest/main.c
@@ -20,7 +20,7 @@ int main()
     }
     system("cls");
     printf("Now you have to do something to meet me, are you ready?\n""y/n\n");
-    scanf("% c", &answer);
+    scanf(" %c", &answer);
     switch(answer)
     {
     case 'y':
@@ -30,39 +30,39 @@ int main()
             printf("Follow the white rabbit...\r");
         }
         system("cls");
-        printf("Suddenly you found out yourself back in the office with th FedEx delivery.\n");
-        printf("There is a cellphone inside.\n");
-        printf("Suspiciously familiar voice telling you're in great danger.\n");
-        printf("Will you follow it's advices?\n");
+        printf("Suddenly you found out yourself back in the office with th FedEx delivery.\n\n");
+        printf("There is a cellphone inside.\n\n");
+        printf("Suspiciously familiar voice telling you're in great danger.\n\n");
+        printf("Will you follow it's advices?\n\n");
         printf("y/n\n");
-        scanf("%.c", &answer);
+        scanf(" %c", &answer);
         switch(answer)
         {
         case 'y':
             system("cls");
-            printf("Crawl through the hall straight to the office\n");
-            printf("There is a hanging stage behind the window\n");
-            printf("Step by step you moving to the stage\n");
-            printf("You're too scared. Will you make the final step on the stage?\ny/n\n");
-            scanf("%.c", &answer);
+            printf("Crawl through the hall straight to the office\n\n");
+            printf("There is a hanging stage behind the window\n\n");
+            printf("Step by step you moving to the stage\n\n");
+            printf("You're too scared. Will you make the final step on the stage?\n\ny/n\n");
+            scanf(" %c", &answer);
             switch(answer)
             {
             case 'y':
                 system("cls");
-                printf("Small step for man , a great step for mankind\n");
+                printf("Small step for man , a great step for mankind\n\n");
                 printf("Your journey just started...");
                 break;
             case 'n':
                 system("cls");
-                printf("Strange people took you to the secret floor.\n");
-                printf("They asked about Morpheus and your hacking life.\n");
-                printf("They grabbed you and implanted under the skin mechanical arthropod.\n");
-                printf("Next morning the strange feeling of surveillance followed you.\n");
-                printf("In the evening the stranger took you to the hood apartments\n");
-                printf("Finally, you met the Morpheus.\n");
-                printf("After the story of the original world and your destination he giving you two pils: red and blue.\n");
+                printf("Strange people took you to the secret floor.\n\n");
+                printf("They asked about Morpheus and your hacking life.\n\n");
+                printf("They grabbed you and implanted under the skin mechanical arthropod.\n\n");
+                printf("Next morning the strange feeling of surveillance followed you.\n\n");
+                printf("In the evening the stranger took you to the hood apartments\n\n");
+                printf("Finally, you met the Morpheus.\n\n");
+                printf("After the story of the original world and your destination he giving you two pils: red and blue.\n\n");
                 printf("r/b\n");
-                scanf("%.c", &answer);
+                scanf(" %c", &answer);
                 switch(answer)
                 {
                 case 'r':
@@ -79,8 +79,8 @@ int main()
             break;
         case 'n':
             system("cls");
-            printf("Maybe you're right and it's easier to live simple stressless life\n");
-            printf("See you next life, Neo...\n");
+            printf("Maybe you're right and it's easier to live simple stressless life\n\n");
+            printf("See you next life, Neo...\n\n");
             break;
         }
         break;
