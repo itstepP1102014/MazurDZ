766a3d07f8ca4a324ce81bad670b5f25d87a3d7c
Kimmazur <holygun187@gmail.com>
2015-01-28 10:51:14 +0000

    modules/

---
+++ modules/include/array.h
@@ -0,0 +1,11 @@
+#ifndef ARRAY_H_INCLUDED
+#define ARRAY_H_INCLUDED
+
+void outputArrray(const int array[], int size);
+void inputArray(int array[], int size);
+int arraySearch(int number, const int array[], int size);
+int minOfArray(const int array[], int sizeOfArray, int sizeOfPart);
+int maxOfArray(const int array[], int sizeOfArray, int sizeOfPart);
+
+
+#endif // ARRAY_H_INCLUDED
