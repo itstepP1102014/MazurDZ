c5e86db71a1db4be3363344de385d1d8f7f3ba3a
Kimmazur <holygun187@gmail.com>
2015-01-29 09:47:59 +0000

    Class Work/

--- Class Work/Classwork28012015/main.c
+++ Class Work/Classwork28012015/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../modules/include/array.h"
+#define N_1000
 
 int main()
 {
-    printf("Hello world!\n");
+    int a[N], n;
+    printf("Enter size of array:\n");
+    scanf("%d", &n);
+
+
     return 0;
 }
