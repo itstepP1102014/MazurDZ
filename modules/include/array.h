2085142193f2451933a7ad46652819037d525ae9
Kimmazur <holygun187@gmail.com>
2015-02-23 18:58:43 +0200

    modules/

--- modules/include/array.h
+++ modules/include/array.h
@@ -1,13 +1,11 @@
 #ifndef ARRAY_H_INCLUDED
 #define ARRAY_H_INCLUDED
 
-#define SIZE 1000
-
-void output_array(const int array[], int size);
-void input_array(int array[], int size);
-int array_search(int number, const int array[], int size);
-int min_of_array(const int array[], int cell);
-int max_of_array(const int array[], int cell);
-void generate_random_numbers_in_array(int array [], int cell);
+void output_array(int array[], int SIZE);
+void input_array(int array[], int SIZE);
+int array_search(int number, const int array[], int SIZE);
+int min_of_array(int array[], int cell);
+int max_of_array(int array[], int cell);
+void generate_random_numbers_in_array(int array[], int cell);
 
 #endif // ARRAY_H_INCLUDED
--- modules/sources/array.c
+++ modules/sources/array.c
@@ -1,66 +1,67 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <assert.h>
-#define SIZE 1000
+#include "../include/array.h"
+#define SIZE 100
 
-void output_array(const int array[], int size)
+void output_array(int *array, int SIZE)
 {
-    for(int i = 0; i < size; ++i)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
+   for(int i = 0; i < SIZE; ++i)
+   {
+       printf("%d\n", array[i]);
+   }
 }
 
-void input_array(int array[], int size)
+void input_array(int *array, int SIZE)
 {
-    for(int i = 0; i < size; ++i)
+    printf("Enter %d elements:\n", SIZE);
+    for(int i = 0; i < SIZE; ++i)
     {
-        printf("Enter element[%d]: ", i);
         scanf("%d", &array[i]);
     }
 }
 
-int array_search(int number, const int array[], int size)
+int array_search(int *array, int SIZE, int number)
 {
-    for(int i = 0; i < size; ++i)
+    for(int i = 0; i < SIZE; ++i)
     {
         if(array[i] == number)
         {
             return i;
         }
+        else
+        {
+            return -1;
+        }
     }
-    return -1;
 }
 
-int min_of_array(const int array[], int cell)
+int min_of_array(int *array, int cell, int min)
 {
     assert(cell > 0 && cell <= SIZE);
-    int min = array[0];
+    min = array[0];
     for(int i = 1; i < cell; ++i)
     {
-        min = array[i] < min? array[i] : min;
+        min = array[i] < min ? array[i] : min;
     }
     return min;
 }
-
-int max_of_array(const int array[], int cell)
+int max_of_array(int *array, int cell, int max)
 {
     assert(cell > 0 && cell <= SIZE);
-    int max = array[0];
+    max = array[0];
     for(int i = 1; i < cell; ++i)
     {
-        max = array[i] > max? array[i] : max;
+        max = array[i] < max ? array[i] : max;
     }
     return max;
 }
-
-void generate_random_numbers_in_array(int array [], int cell)
+void generate_random_numbers_in_array(int *array, int cell)
 {
     srand(time(NULL));
-    assert(cell > 0 && cell <= SIZE);
-    for(int i = 0; i < cell; ++i)
+    for(int i = 0; i < SIZE; ++i)
     {
-        array[i] = rand()% 10;
+        array[i] = rand() % 100;
     }
 }
