329c10cbf38d4d2611e68d5b9a53df87a76c652d
Kimmazur <holygun187@gmail.com>
2015-03-01 08:45:10 +0200

    Self practice/

--- Self practice/mir_koda_task1/main.c
+++ Self practice/mir_koda_task1/main.c
@@ -6,7 +6,7 @@ int main()
     double date_of_searcing, date_of_birth;
     printf("Enter the current or various date\n"
            "And the person's date of birth:\n");
-    scanf("%d %d", &date_of_searcing, &date_of_birth);
-    printf("Current age of person is %d", date_of_searcing - date_of_birth);
+    scanf("%f %f", &date_of_searcing, &date_of_birth);
+    printf("Current age of person is %.2f", date_of_searcing - date_of_birth);
     return 0;
 }
