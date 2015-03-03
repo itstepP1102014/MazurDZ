98e8e4862e62dd1af44cf2f7acd33b3f97ce14d1
Kimmazur <holygun187@gmail.com>
2015-03-03 10:43:05 +0200

    Self practice/

--- Self practice/mir_koda_task2/main.c
+++ Self practice/mir_koda_task2/main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    int n;
+    int n, units, scores, hundreds;
     scanf("%d", &n);
-    n = n % 10;
-    printf("%d", n);
+    units = n % 10;
+    scores = (n % 100)/10;
+    hundreds = n / 100;
+
+    if(units == scores)
+    {
+        printf("Units and scores are the same numbers\n");
+    }
+    if(units == hundreds)
+    {
+        printf("Units and hundreds are the same numbers\n");
+    }
+    if(scores == hundreds)
+    {
+        printf("Scores and hundreds are the same numbers\n");
+    }
 
     return 0;
 }
