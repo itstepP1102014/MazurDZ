75d4772ef55cf76668cd938c89b5185489c59aa1
Kimmazur <holygun187@gmail.com>
2015-03-06 16:52:49 +0200

    Self practice/

---
+++ Self practice/mir_koda_task4/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int output_number(int n)
+{
+    int a, b, c;
+    c = n % 10;
+    b = (n % 100)/10;
+    a = n / 100;
+
+    if(a != b && a != c && b != c)
+        return n;
+}
+
+int main()
+{
+    int n = 103;
+
+    for(; n > 102 && n <987; ++n)
+    {
+        output_number(n);
+    }
+    printf("%d\n", n);
+
+    return 0;
+}
